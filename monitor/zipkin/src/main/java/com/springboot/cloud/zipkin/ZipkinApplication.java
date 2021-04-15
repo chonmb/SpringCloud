6c74feb208023690a84fa94a4d9417fcb03f299e
jiangxin <jiangxin2@shanghai-electric.com>
2021-04-15 10:07:22 +0800

    add zipkin for tracing analysis

---
+++ monitor/zipkin/src/main/java/com/springboot/cloud/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * @author chonmb Email:jiangxin2@shanghai-electric.com
+ * @date 2021/4/15 8:46
+ */
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
